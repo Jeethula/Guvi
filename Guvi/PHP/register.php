337d84de1b6842932b2374a4d9ca4f32a6c8f099
Jeethu_LA <113278334+Jeethula@users.noreply.github.com>
2024-03-22 15:39:59 +0530

    Update register.php

--- Guvi/PHP/register.php
+++ Guvi/PHP/register.php
@@ -1,11 +1,11 @@
 <?php
 
-$servername = "localhost:3306";
-$username = "root";
-$password = "8903942007";
-$dbname = "jeethu";
+$servername = "mysql5048.site4now.net";
+$username_db = "aa6c48_guvi123";
+$password_db = "Password@123";
+$dbname = "db_aa6c48_guvi123";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username_db, $password_db, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -13,11 +13,12 @@ if ($conn->connect_error) {
 
 $username = $_POST['email'];
 $password = $_POST['password'];
+$user_id = rand(10,99999);
 
-$sql = "INSERT INTO user (username, password) VALUES (?, ?)";
+$sql = "INSERT INTO user (username, password,user_id) VALUES (?, ?,?)";
 $stmt = $conn->prepare($sql);
 
-$stmt->bind_param("ss", $username, $password);
+$stmt->bind_param("sss", $username, $password,$user_id);
 
 if ($stmt->execute()) {
     echo "New record created successfully";
